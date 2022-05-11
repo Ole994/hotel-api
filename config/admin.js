435f277e39d57f975402f4f43a7ec57b54e30895
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-05-11 16:33:20 +0200

    contact added

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '09ca892acde2668daacadc71c042c334'),
+    secret: env("ADMIN_JWT_SECRET", "09ca892acde2668daacadc71c042c334"),
   },
 });
